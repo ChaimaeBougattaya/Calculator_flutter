1adafbf6e2fe89ad3ecb98a3d55cdc749f7be575
Chaimae Bougattaya <chaimaebougattaya1@gmail.com>
2022-08-26 19:59:53 +0100

    commit4

--- lib/Screens/Calculator.Screen.dart
+++ lib/Screens/Calculator.Screen.dart
@@ -1,5 +1,6 @@
 import 'package:calculator_app/Widgets/Buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({Key? key}) : super(key: key);
@@ -9,16 +10,29 @@ class CalculatorScreen extends StatefulWidget {
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
-
-  var question="question";
-  var answer="answer";
-  final List<String> buttons=
-  [
-    "C" , "DEL" , "%" , "/" ,
-    "9" , "8" , "7" , "×" ,
-    "6" , "5" , "4" , "-" ,
-    "3" , "2" , "1" , "+" ,
-    "0" , "." , "ANS" , "="
+  var question = "";
+  var answer = "";
+  final List<String> buttons = [
+    "C",
+    "DEL",
+    "%",
+    "/",
+    "9",
+    "8",
+    "7",
+    "×",
+    "6",
+    "5",
+    "4",
+    "-",
+    "3",
+    "2",
+    "1",
+    "+",
+    ".",
+    "0",
+    "ANS",
+    "="
   ];
 
   @override
@@ -29,53 +43,123 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
         children: <Widget>[
           Expanded(
               child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Text(question),
-                    Text(answer)
-                  ],
-                ),
-              )
-          ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                SizedBox(height: 50,),
+                Container(
+                    padding  : EdgeInsets.all(20),
+                    alignment: Alignment.centerLeft,
+                    child: Text(question,style: const TextStyle(fontSize: 20),)),
+                Container(
+                    padding: EdgeInsets.all(20),
+                    alignment: Alignment.centerRight,
+                    child: Text(answer,style: const TextStyle(fontSize: 20),))
+              ],
+            ),
+          )),
           Expanded(
               flex: 2,
               child: Container(
                 child: GridView.builder(
-                  itemCount: buttons.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-                    itemBuilder: (BuildContext context,int index) {
+                    itemCount: buttons.length,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4),
+                    itemBuilder: (BuildContext context, int index) {
                       return MyButton(
-                        colorbutton: isOperator(buttons[index]) ? Colors.deepPurple :
-                                    isDelete(buttons[index]) ? Colors.green :
-                                    isClear(buttons[index]) ? Colors.red : Colors.deepPurple[50],
-                        colortext: isOperator(buttons[index]) || isDelete(buttons[index]) || isClear(buttons[index])  ? Colors.white
-                                  : Colors.deepPurple,
+                        isTapped: () {
+                          setState(() {
+                            if(index==0){
+                              question="";
+                              answer="";
+                            }else if(index==1){
+                              if(!question.isEmpty){
+                                question=question.substring(0,question.length-1);
+                              }
+                            }
+                            else{
+                              if(question.isEmpty){
+                                if(this.isNumber(buttons[index])){
+                                  question+=buttons[index];
+                                  //answer=buttons[index];
+                                }
+                                else
+                                  {
+                                    Fluttertoast.showToast(
+                                        msg: "invalid format used",
+                                        backgroundColor: Colors.black87,
+                                        fontSize: 16.0
+                                    );
+                                  }
+                              }else{
+                                if(this.end_with_operator(question)){
+                                  if(this.isOperator(buttons[index])) {
+                                    Fluttertoast.showToast(
+                                        msg: "invalid format used",
+                                        backgroundColor: Colors.black87,
+                                        fontSize: 16.0
+                                    );
+                                  } else
+                                    question+=buttons[index];
+                                }else{
+                                  question+=buttons[index];
+                                }
+                              }
+                            }
+                          });
+                        },
+                        colorbutton: isOperator(buttons[index])
+                            ? Colors.deepPurple
+                            : isDelete(buttons[index])
+                                ? Colors.green
+                                : isClear(buttons[index])
+                                    ? Colors.red
+                                    : Colors.deepPurple[50],
+                        colortext: isOperator(buttons[index]) ||
+                                isDelete(buttons[index]) ||
+                                isClear(buttons[index])
+                            ? Colors.white
+                            : Colors.deepPurple,
                         textbutton: buttons[index],
-                  );
-                })
-               ,
+                      );
+                    }),
               ))
         ],
       ),
     );
   }
 
-  bool isOperator(String str){
-    return ( str == "%" || str == "×" ||
-              str == "-" || str == "+" ||
-              str == "/" || str == "=");
+  bool isOperator(String str) {
+    return (str == "%" ||
+        str == "×" ||
+        str == "-" ||
+        str == "+" ||
+        str == "/" ||
+        str == "=");
   }
 
-  bool isNumber(String str){
-    return ( str == "0" || str == "1" || str == "2" ||
-              str == "3" || str == "4" || str == "5" ||
-              str == "6" || str == "7" || str == "8" || str == "9");
+  bool isNumber(String str) {
+    return (str == "0" ||
+        str == "1" ||
+        str == "2" ||
+        str == "3" ||
+        str == "4" ||
+        str == "5" ||
+        str == "6" ||
+        str == "7" ||
+        str == "8" ||
+        str == "9");
   }
-  bool isDelete(String str){
-    return str=="DEL";
+
+  bool isDelete(String str) {
+    return str == "DEL";
   }
-  bool isClear(String str){
-    return str=="C";
+
+  bool isClear(String str) {
+    return str == "C";
+  }
+  bool end_with_operator(String str){
+    return str.endsWith("%") || str.endsWith("×") || str.endsWith("-") ||
+        str.endsWith("+") ||str.endsWith("/");
   }
 }
--- lib/Widgets/Buttons.dart
+++ lib/Widgets/Buttons.dart
@@ -3,28 +3,31 @@ class MyButton extends StatelessWidget {
   final colorbutton,        //Color of button
         colortext;          //color of text
   final String textbutton;  // text of button
-
-  MyButton({ this.colorbutton, this.colortext, required this.textbutton});
+  final isTapped;
+  MyButton({ this.colorbutton, this.colortext, required this.textbutton, this.isTapped});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Container(
-            color: colorbutton,
-            child: Center(
-              child: Text(
-                  textbutton,
-                  style: TextStyle(
-                      color: colortext,
-                    fontSize: 22
-                  )
+    return GestureDetector(
+      onTap: isTapped,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(50),
+          child: Container(
+              color: colorbutton,
+              child: Center(
+                child: Text(
+                    textbutton,
+                    style: TextStyle(
+                        color: colortext,
+                      fontSize: 22
+                    )
+                ),
               ),
             ),
           ),
-        ),
+      ),
     );
   }
 }
